5ac12ba8a182fa9614d120195ca07d243bff1b17
Andrei <samplec0de@icloud.com>
2026-07-11 00:02:59 +0300

    feat(ha): constants and config_flow manifest flag

--- custom_components/sim800c/const.py
+++ custom_components/sim800c/const.py
@@ -2,10 +2,18 @@
 
 from logging import Logger, getLogger
 
+from homeassistant.const import Platform
+
 LOGGER: Logger = getLogger(__package__)
 
 DOMAIN = "sim800c"
 
-# Configuration constants
 CONF_BAUD_RATE = "baud_rate"
 DEFAULT_BAUD_RATE = 9600
+
+SERVICE_SEND_SMS = "send_sms"
+ATTR_TARGET = "target"
+ATTR_MESSAGE = "message"
+ATTR_FORCE_UNICODE = "force_unicode"
+
+PLATFORMS = [Platform.SENSOR]
